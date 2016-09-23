404213c5a3060b0c304c1ebc53269e0c29cb600c
woniper <leekw3747@gmail.com>
2016-09-24 00:55:18 +0900

    searcher2

---
+++ src/test/java/net/woniper/tdd/test/searcher/searcher2/Searcher.java
@@ -0,0 +1,86 @@
+package net.woniper.tdd.test.searcher.searcher2;
+
+/**
+ * Created by woniper on 2016. 9. 23..
+ */
+public class Searcher {
+
+    private String text;
+
+    public Searcher(String text) {
+        if(isEmptyOrNull(text))
+            throw new IllegalArgumentException();
+
+        this.text = text;
+    }
+
+    public String search(String keyword) {
+        if(isEmptyOrNull(keyword))
+            throw new IllegalArgumentException();
+
+        if(isKeywordGreatThenText(keyword))
+            throw new IllegalArgumentException();
+
+        if(!isKeywordContainsInText(keyword))
+            return null;
+
+        String prefix = getTextPrefix(keyword);
+        String searchText = getSearchText(keyword);
+        String suffix = getTextSuffix(keyword);
+
+        if(containsPrefixNumberOrAlphabetOrUnderscore(prefix))
+            return null;
+
+        if(containsSuffixNumberOrAlphabetOrUnderscore(suffix))
+            return null;
+
+        return makeSearchText(prefix, searchText, suffix);
+    }
+
+    private String makeSearchText(String prefix, String searchText, String suffix) {
+        return prefix + "{" + searchText + "}" + suffix;
+    }
+
+    private String getTextPrefix(String keyword) {
+        return text.substring(0, text.indexOf(keyword));
+    }
+
+    private String getSearchText(String keyword) {
+        return text.substring(text.indexOf(keyword), text.indexOf(keyword) + keyword.length());
+    }
+
+    private String getTextSuffix(String keyword) {
+        return text.substring(text.indexOf(keyword) + keyword.length(), text.length());
+    }
+
+    private boolean containsSuffixNumberOrAlphabetOrUnderscore(String suffix) {
+        if(isEmptyOrNull(suffix))
+            return false;
+
+        return containsNumberOrAlphabetOrUnderscore(suffix.substring(0, 1));
+    }
+
+    private boolean containsPrefixNumberOrAlphabetOrUnderscore(String prefix) {
+        if(isEmptyOrNull(prefix))
+            return false;
+
+        int prefixLength = prefix.length();
+        return containsNumberOrAlphabetOrUnderscore(prefix.substring(prefixLength - 1, prefixLength));
+    }
+
+    private boolean containsNumberOrAlphabetOrUnderscore(String text) {
+        return text.matches("[0-9a-zA-Z]") || "_".equals(text);
+    }
+
+    private boolean isKeywordContainsInText(String keyword) {
+        return text.contains(keyword);
+    }
+
+    private boolean isEmptyOrNull(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private boolean isKeywordGreatThenText(String keyword) {
+        return keyword.length() > text.length();
+    }
+}
--- src/test/java/net/woniper/tdd/test/searcher/searcher2/SearcherTest.java
+++ src/test/java/net/woniper/tdd/test/searcher/searcher2/SearcherTest.java
@@ -2,41 +2,107 @@ package net.woniper.tdd.test.searcher.searcher2;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.isA;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by woniper on 2016. 9. 23..
  *
- * 테스트 메소드를 생각나는대로 모두 정의해보자. (todo처럼)
+ * 테스트 메소드를 생각나는대로 모두 정의한 후 TDD 해보자. (todo처럼)
  */
 public class SearcherTest {
 
     // 생성자 argument Text Empty String, Null 테스트
     @Test
     public void testSearcherConstructorArgumentValidation() throws Exception {
+        assertSearcherConstructorArgument("");
+        assertSearcherConstructorArgument(null);
+    }
+
+    private void assertSearcherConstructorArgument(String text) {
+        IllegalArgumentException exception = null;
+
+        try {
+            new Searcher(text);
+        } catch (Exception e) {
+            exception = (IllegalArgumentException) e;
+        }
+
+        assertThat(exception, isA(IllegalArgumentException.class));
     }
 
     // search method keyword Empty String, Null 테스트
     @Test
     public void testSearchEmptyOrNullValidation() throws Exception {
+        assertSearchKeyword("");
+        assertSearchKeyword(null);
+    }
+
+    private void assertSearchKeyword(String keyword) {
+        IllegalArgumentException exception = null;
+
+        try {
+            new Searcher("flex").search(keyword);
+        } catch (Exception e) {
+            exception = (IllegalArgumentException) e;
+        }
+
+        assertThat(exception, isA(IllegalArgumentException.class));
     }
 
     // search method Text와 Keyword 길이 테스트 (text가 keyword에 길이보다 보다 크거나 같아야한다)
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testTextAndKeywordLengthValidation() throws Exception {
+        new Searcher("1").search("12");
+    }
+
+    // search method text에 keyword 문자열이 포함되어있는지 테스트
+    @Test
+    public void testKeywordContainsInTextValidation() throws Exception {
+        // given
+        Searcher searcher = new Searcher("no a flex");
+
+        // when
+        String expected = searcher.search("flexing");
+
+        // then
+        assertNull(expected);
     }
 
     // search method keyword에 앞 또는 뒤에 숫자가 포함되었는지 테스트
     @Test
     public void testKeywordNumberTextValidation() throws Exception {
+        assertSearchResultIsNull("0flex");
+        assertSearchResultIsNull("flex0");
     }
 
     // search method keyword에 앞 또는 뒤에 알파벳가 포함되었는지 테스트
     @Test
-    public void testKeywordAlpahbetTextValidation() throws Exception {
+    public void testKeywordAlphabetTextValidation() throws Exception {
+        assertSearchResultIsNull("Aflex");
+        assertSearchResultIsNull("flexB");
+        assertSearchResultIsNull("aflex");
+        assertSearchResultIsNull("flexb");
     }
 
     // search method keyword에 앞 또는 뒤에 "_"가 포함되었는지 테스트
     @Test
     public void testKeywordUnderscoreTextValidation() throws Exception {
+        assertSearchResultIsNull("_flex");
+        assertSearchResultIsNull("flex_");
+    }
+
+    private void assertSearchResultIsNull(String text) {
+        // given
+        Searcher searcher = new Searcher(text);
+
+        // when
+        String expected = searcher.search("flex");
+
+        // then
+        assertNull(expected);
     }
 
     // 예시로 주어진 keyword 테스트
@@ -48,6 +114,9 @@ public class SearcherTest {
      */
     @Test
     public void testExampleKeywordIsNull() throws Exception {
+        assertSearchResultIsNull("reflex");
+        assertSearchResultIsNull("yes, flex1");
+        assertSearchResultIsNull("no, flexible");
     }
 
     // 예시로 주어진 keyword 테스트
@@ -59,5 +128,19 @@ public class SearcherTest {
      */
     @Test
     public void testExampleKeywordIsNotNull() throws Exception {
+        assertSearchResultIsNotNull("flex?", "{flex}?");
+        assertSearchResultIsNotNull("(flex)", "({flex})");
+        assertSearchResultIsNotNull("no a flex", "no a {flex}");
+    }
+
+    private void assertSearchResultIsNotNull(String text, String actual) {
+        // given
+        Searcher searcher = new Searcher(text);
+
+        // when
+        String expected = searcher.search("flex");
+
+        // then
+        assertEquals(expected, actual);
     }
 }
