f4f5b2fcab9ee4ec585501aa67fd12f84b42e930
woniper <leekw3747@gmail.com>
2016-09-30 21:05:24 +0900

    문서 정리

--- src/test/java/net/woniper/tdd/test/calculator/calculator2/CalculatorTest2.java
+++ src/test/java/net/woniper/tdd/test/calculator/calculator2/CalculatorTest2.java
@@ -4,4 +4,13 @@ package net.woniper.tdd.test.calculator.calculator2;
  * Created by woniper on 2016. 9. 26..
  */
 public class CalculatorTest2 {
+
+    // constructor argument null, length 체크
+    // add method empty string, null 체크
+    // add method result5 테스트
+    // add method result13 테스트
+    // add method result100 테스트
+    // add method result103 테스트
+    // isContainsSeperator method 테스트
+
 }
