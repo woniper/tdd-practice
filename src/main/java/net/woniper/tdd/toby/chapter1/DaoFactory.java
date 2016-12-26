e28d01a7c7f6ed68d7416e1473502cbef53d982c
woniper <leekw3747@gmail.com>
2016-12-26 16:29:59 +0900

    chapter1 Datasource interface로 변경

--- src/main/java/net/woniper/tdd/toby/chapter1/DaoFactory.java
+++ src/main/java/net/woniper/tdd/toby/chapter1/DaoFactory.java
@@ -4,6 +4,9 @@ import net.woniper.tdd.toby.chapter1.connection.ConnectionMaker;
 import net.woniper.tdd.toby.chapter1.connection.DConnectionMaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * Created by woniper on 2016. 12. 26..
@@ -13,7 +16,21 @@ public class DaoFactory {
 
     @Bean
     public UserDao userDao() {
-        return new UserDao(connectionMaker());
+        UserDao userDao = new UserDao();
+        userDao.setDataSource(dataSource());
+        return userDao;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
+        dataSource.setUsername("root");
+        dataSource.setPassword("q1w2e3");
+
+        return dataSource;
     }
 
     @Bean
--- src/main/java/net/woniper/tdd/toby/chapter1/UserDao.java
+++ src/main/java/net/woniper/tdd/toby/chapter1/UserDao.java
@@ -1,32 +1,24 @@
 package net.woniper.tdd.toby.chapter1;
 
-import net.woniper.tdd.toby.chapter1.connection.ConnectionMaker;
-
-import java.sql.*;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by woniper on 2016. 12. 26..
  */
 public class UserDao {
 
-    /**
-     * interface를 구현한 구현체를 주입받기 위함
-     * UserDao는 DConnectionMaker인지 NConnectionMaker인지 알 필요가 없다.
-     */
-    private ConnectionMaker connectionMaker;
+    private DataSource dataSource;
 
-    /**
-     * 생성자를 통한 주입
-     * @see net.woniper.tdd.toby.chapter1.connection.DConnectionMaker
-     * @see net.woniper.tdd.toby.chapter1.connection.NConnectionMaker
-     * @param connectionMaker
-     */
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
         ps.setString(1, user.getId());
@@ -40,7 +32,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
