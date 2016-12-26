f6daf56e9c10bbfe475b566cbd4bfed93a800f51
woniper <leekw3747@gmail.com>
2016-12-26 14:10:42 +0900

    chapter1 Connection 중복 생성, 메소드 추출 기법으로 분리

--- src/main/java/net/woniper/tdd/toby/chapter1/UserDao.java
+++ src/main/java/net/woniper/tdd/toby/chapter1/UserDao.java
@@ -8,8 +8,7 @@ import java.sql.*;
 public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "q1w2e3");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
         ps.setString(1, user.getId());
@@ -23,8 +22,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "q1w2e3");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
@@ -42,5 +40,17 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    /**
+     * 중복 Connection 생성을 메소드로 분리
+     * 메소드 추출 기법 (extract method)
+     * @return
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "q1w2e3");
     }
 }
