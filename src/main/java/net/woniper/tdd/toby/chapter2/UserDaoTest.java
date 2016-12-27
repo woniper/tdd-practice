c2a7befb56c9c5693aa9044b6a75592241e9dcb3
woniper <leekw3747@gmail.com>
2016-12-27 16:43:07 +0900

    chapter2 addAndGet 테스트 보완

--- src/main/java/net/woniper/tdd/toby/chapter2/UserDaoTest.java
+++ src/main/java/net/woniper/tdd/toby/chapter2/UserDaoTest.java
@@ -18,21 +18,23 @@ public class UserDaoTest {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 
         UserDao dao = context.getBean(UserDao.class);
-        User user1 = new User("woniper", "이경원", "1234");
-        User user2 = new User("01b", "박영일", "1234");
-        User user3 = new User("hansin", "한신", "1234");
+        User user1 = new User("woniper", "kw", "1234");
+        User user2 = new User("01b", "yi", "1234");
 
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
         dao.add(user1);
-        assertThat(dao.getCount(), is(1));
-
         dao.add(user2);
         assertThat(dao.getCount(), is(2));
 
-        dao.add(user3);
-        assertThat(dao.getCount(), is(3));
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(), is(user1.getPassword()));
+
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassword(), is(user2.getPassword()));
 
     }
 }
