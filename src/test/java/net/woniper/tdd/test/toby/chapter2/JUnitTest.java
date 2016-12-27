4ff289e1801e56ee93dcf31dbfd97494b596f95b
woniper <leekw3747@gmail.com>
2016-12-27 17:42:01 +0900

    chapter2 JUnit 테스트 오브젝트 테스트2

--- src/test/java/net/woniper/tdd/test/toby/chapter2/JUnitTest.java
+++ src/test/java/net/woniper/tdd/test/toby/chapter2/JUnitTest.java
@@ -2,6 +2,9 @@ package net.woniper.tdd.test.toby.chapter2;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
@@ -11,23 +14,23 @@ import static org.junit.Assert.assertThat;
  */
 public class JUnitTest {
 
-    static JUnitTest testObject;
+    static Set<JUnitTest> testObjects = new HashSet<>();
 
     @Test
     public void test1() throws Exception {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test
     public void test2() throws Exception {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test
     public void test3() throws Exception {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 }
