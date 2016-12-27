829251754aa6eee8d55d55ac7dabac29d569878c
woniper <leekw3747@gmail.com>
2016-12-27 16:39:27 +0900

    chapter2 포괄적인 테스트

--- src/main/java/net/woniper/tdd/toby/chapter2/User.java
+++ src/main/java/net/woniper/tdd/toby/chapter2/User.java
@@ -9,6 +9,14 @@ public class User {
     String name;
     String password;
 
+    public User() {}
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
     public String getId() {
         return id;
     }
--- src/main/java/net/woniper/tdd/toby/chapter2/UserDaoTest.java
+++ src/main/java/net/woniper/tdd/toby/chapter2/UserDaoTest.java
@@ -16,21 +16,23 @@ public class UserDaoTest {
     @Test
     public void addAndGet() throws SQLException, ClassNotFoundException {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
         UserDao dao = context.getBean(UserDao.class);
+        User user1 = new User("woniper", "이경원", "1234");
+        User user2 = new User("01b", "박영일", "1234");
+        User user3 = new User("hansin", "한신", "1234");
 
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
-        User user = new User();
-        user.setId("kyung-won");
-        user.setName("kw");
-        user.setPassword("1234");
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
 
-        dao.add(user);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        User user2 = dao.get(user.getId());
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
 
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
     }
 }
