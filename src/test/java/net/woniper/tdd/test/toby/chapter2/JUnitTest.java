0100c0aecb0b5d4b71d11e35ca69ab86c0e0e793
woniper <leekw3747@gmail.com>
2016-12-27 18:02:27 +0900

    chapter2 스프링 테스트 컨텍스트 테스트

--- src/test/java/net/woniper/tdd/test/toby/chapter2/JUnitTest.java
+++ src/test/java/net/woniper/tdd/test/toby/chapter2/JUnitTest.java
@@ -1,6 +1,12 @@
 package net.woniper.tdd.test.toby.chapter2;
 
+import net.woniper.tdd.toby.chapter2.DaoFactory;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -12,25 +18,39 @@ import static org.junit.Assert.assertThat;
  * 학습 테스트
  * Created by woniper on 2016. 12. 27..
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = DaoFactory.class)
 public class JUnitTest {
 
+    @Autowired private ApplicationContext context;
+
     static Set<JUnitTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
     @Test
     public void test1() throws Exception {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
     @Test
     public void test2() throws Exception {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
     @Test
     public void test3() throws Exception {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+
+        assertThat(contextObject, either(is(nullValue())).or(is(this.context)));
+        contextObject = this.context;
     }
 }
